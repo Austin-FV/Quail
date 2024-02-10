4e7dfe966d504a210fccd03aaed2bf3156f1d927
Austin-FV <austin.varghese7@gmail.com>
2024-02-09 20:13:28 -0500

    [#9] created page redirect for dashboard if not signed in to go to homepage, created a quiz me card that will direct user to the quiz page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,37 @@
+import QuizMeCard from '@/components/dashboard/QuizMeCard';
+import { getAuthSession } from '@/lib/nextauth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 type Props = {}
 
-const Dashboard = (props: Props) => {
-  return (
-    <div>Dashboard</div>
-  )
+export const metadata = {
+    title: "Dashboard | Quail"
+}
+
+const Dashboard = async (props: Props) => {
+    const session = await getAuthSession();
+    if (!session?.user){
+        // if user is not logged in then send them to home page
+        redirect('/');
+    }
+    return (
+        <main className='p-8 mx-auto max-w-7xl'>
+            <div className='flex items-center'>
+                <h2 className='mr-2 text-3xl font-bold tracking-tight'>Dashboard</h2>
+            </div>
+
+            <div className="grid gap-4 mt-4 md:grid-cols-2">
+                <QuizMeCard />
+                {/* <HistoryCard /> */}
+            </div>
+
+            <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+
+            </div>
+
+        </main>
+    )
 }
 
 export default Dashboard;
